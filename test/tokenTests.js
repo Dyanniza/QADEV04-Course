f54da76559e0e85e6df7ac47f7116337bf5dde18
jfvf <jimmy.freddy.vargas@gmail.com>
2015-10-13 18:32:44 -0400

    initiating the qadev04 project

---
+++ test/tokenTests.js
@@ -0,0 +1,40 @@
+//Token tests
+
+var expect =  require('chai').expect;
+var request = require('superagent');
+require('superagent-proxy')(request);
+
+describe('Token',function(){
+	this.timeout(4000);
+
+
+	it('GET /Authentication/token',function(done){
+		request
+			.get('https://todo.ly/api/authentication/token.json')
+			.proxy('http://172.20.240.5:8080')
+			.auth('gordines007@gmail.com','control123!@#')
+		.end(function(err, res){
+			console.log(res.body);
+			console.log(res.status);
+			
+			expect(res.body.TokenString).not.to.be.null;
+			expect(res.body.UserEmail).to.equal('gordines007@gmail.com');
+
+			var regExpress = '[0-9]+';
+			var expireDate = res.body.ExpirationTime.match(regExpress);
+
+			var date = new Date();
+			var currentDate = date.getTime();
+
+			var diffTime = expireDate - currentDate;
+			var calcHours = diffTime/(1000*60*60);
+
+			console.log('+++' + diffTime);
+			console.log(calcHours);
+
+			expect(calcHours).to.be.below(4).and.to.be.above(3.9);
+			done();
+		});
+	});
+});
+

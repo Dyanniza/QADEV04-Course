524415b32667125b863e5b74fe0c69ff574fa135
jfvf <jimmy.freddy.vargas@gmail.com>
2015-10-19 14:56:55 -0400

    reorganizing the tests

--- test/accProjectsv2.0.js
+++ test/accProjectsv2.0.js
@@ -16,14 +16,40 @@ describe('CRUD operation over Todo.ly Projects',function(){
 		userPassword:'control123!@#'
 	};
 	var tk = null;
-
+	var projectId = null;
+	
 	before('Getting the token',function(done){
-		//getting the token
 		tokenAPI
 			.get(userCredential,function(res){
 				tk = res.body;
 				expect(tk.UserEmail).to.equal(userCredential.userAccount);
 				done();
+			});
+	});
+	
+	after('Deleting the token',function(done){
+		tokenAPI
+			.delete(tk,function(res){	
+				var tkDeleted = res.body;
+				expect(tkDeleted.TokenString).to.equal(tk.TokenString);
+				tk = null;
+				done();
+			});
+	});
+	
+	afterEach('Delete Project',function(done){
+		//deleting the project
+		projectsAPI
+			.delete(projectId,tk.TokenString,function(res){
+			
+				var prjDeleted = res.body;
+				expect(res.status).to.equal(200);
+				expect(prjDeleted.Id).to.equal(projectId);
+				expect(prjDeleted.Deleted).to.be.true;
+				
+				projectId = null;
+				done();
+											
 			});
 	});
 	
@@ -37,9 +63,10 @@ describe('CRUD operation over Todo.ly Projects',function(){
 		projectsAPI
 			.create(prj,tk.TokenString,function(res){
 						var prjCreated = res.body;
+						projectId = prjCreated.Id;
 						var lastSyncedDate = dateHandler.getDateFromUnixTimeStamp(prjCreated.LastSyncedDateTime.match('[0-9]+'));//the date in milisecond it is in first position
 						var currentDate = dateHandler.getDateFromUnixTimeStamp((new Date()).getTime());
-
+						
 						expect(res.status).to.equal(200);
 						expect(prjCreated.Id).not.to.be.null;
 						expect(prjCreated.Content).to.equal(prj.Content);
@@ -49,79 +76,59 @@ describe('CRUD operation over Todo.ly Projects',function(){
 						expect(prjCreated.Children).to.deep.equal([]);
 						expect(lastSyncedDate).to.equal(currentDate);
 						//many other assertions
-						
-
-						//deleting the project
-						projectsAPI
-							.delete(prjCreated.Id,tk.TokenString,function(res){
-								var prjDeleted = res.body;
-								expect(res.status).to.equal(200);
-								expect(prjDeleted.Id).to.equal(prjCreated.Id);
-								expect(prjDeleted.Deleted).to.be.true;
-
-								tokenAPI
-									.delete(tk,function(res){	
-										var tkDeleted = res.body;
-										expect(tkDeleted.TokenString).to.equal(tk.TokenString);
-										tk = null;
-										done();
-									});							
-							});
-
+						done();
 					});
 			
 	});
 
-	//assuming the pre and pos conditions don't need assertions
-	it('PUT /projects/[id].json returns 200',function(done){
-		
-		//create a project
+	describe('Read and Edit operations',function(){
 		var bProject = {
 			Content:'Base Project'
 		};
-
-		projectsAPI
-			.create(bProject,tk.TokenString,function(res){
-							var prjCreated = res.body;
-
-							//editing the project
-							var eProject = {
-								Content : 'Base Project edited',
-								Icon: 1,							
-							};
-							projectsAPI
-								.edit(prjCreated.Id,eProject,tk.TokenString,function(res){
-
-									var editProject = res.body;
-									var lastSyncedDate = dateHandler.getDateFromUnixTimeStamp(editProject.LastSyncedDateTime.match('[0-9]+'));//the date in milisecond it is in first position
-									var currentDate = dateHandler.getDateFromUnixTimeStamp((new Date()).getTime());
-
-									expect(res.status).to.equal(200);
-									expect(editProject.Id).to.equal(prjCreated.Id);
-									expect(editProject.Content).to.equal(eProject.Content);
-									expect(editProject.ItemsCount).to.equal(0);
-									expect(editProject.Icon).to.equal(eProject.Icon);
-									expect(editProject.Deleted).to.be.false;
-									expect(editProject.Children).to.deep.equal([]);
-									expect(lastSyncedDate).to.equal(currentDate);
-									//many other assertions
-
-									//deleting the project
-									projectsAPI
-										.delete(editProject.Id,tk.TokenString,function(res){
-
-											//deleting the token
-											tokenAPI
-												.delete(tk,function(res){
-													done();
-											});
-									});
-							});
-					
-				});
-
 		
+		beforeEach('Creating a project base',function(done){
+			projectsAPI
+				.create(bProject,tk.TokenString,function(res){
+					projectId = res.body.Id;
+					done();
+				});
+		});
+		
+		//assuming the pre and pos conditions don't need assertions
+		it('PUT /projects/[id].json returns 200',function(done){
+		
+			
+			//editing the project
+			var eProject = {
+				Content : 'Base Project edited',
+				Icon: 1,							
+			};
+			
+			projectsAPI
+				.edit(prjCreated.Id,eProject,tk.TokenString,function(res){
+
+					var editProject = res.body;
+					var lastSyncedDate = dateHandler.getDateFromUnixTimeStamp(editProject.LastSyncedDateTime.match('[0-9]+'));//the date in milisecond it is in first position
+					var currentDate = dateHandler.getDateFromUnixTimeStamp((new Date()).getTime());
+
+					expect(res.status).to.equal(200);
+					expect(editProject.Id).to.equal(prjCreated.Id);
+					expect(editProject.Content).to.equal(eProject.Content);
+					expect(editProject.ItemsCount).to.equal(0);
+					expect(editProject.Icon).to.equal(eProject.Icon);
+					expect(editProject.Deleted).to.be.false;
+					expect(editProject.Children).to.deep.equal([]);
+					expect(lastSyncedDate).to.equal(currentDate);
+					//many other assertions
+					done();
+			});
+		});
+		
+		it('GET /projects/[id].json returns 200',function(){
+		
+		});
 	});
+	
 });
 
 
